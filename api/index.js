446c9c898e24c0de9e8ee890ec6af72a99694a6b
Gokulcasper <ronitcasper215@gmail.com>
2022-08-26 16:40:56 +0530

    Feat : using JWT & Cookie Parser for Security

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import authRoute from "./routes/auth.js"
 import usersRoute from "./routes/users.js"
 import hotelsRoute from "./routes/hotels.js"
 import roomsRoute from "./routes/rooms.js"
+import cookieParser from "cookie-parser";
 
 
 const app = express()
@@ -25,7 +26,7 @@ mongoose.connection.on("disconnected", () => {
 })
 
 //Middlewares
-
+app.use(cookieParser())
 app.use(express.json())
 
 

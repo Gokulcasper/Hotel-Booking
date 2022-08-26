fa4122aee3d6aae70247634cd0ec8e6c6186cb0c
Gokulcasper <ronitcasper215@gmail.com>
2022-08-26 19:36:42 +0530

    Feat : login complete

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,6 +1,31 @@
 import express from "express"
+import { deleteUser, getUser, getUsers, updateUser } from "../controllers/user.js"
+import { verifyAdmin, verifyToken, verifyUser } from "../utils/verifyToken.js";
 
 const router = express.Router()
 
+router.get("/checkauthentication", verifyToken, (req, res, next) => {
+    res.send("hello user, you are logged in")
+})
+
+router.get("/checkuser/:id", verifyUser, (req, res, next) => {
+    res.send("hello user, you are logged in and you can delete account")
+})
+
+router.get("/checkadmin/:id", verifyAdmin, (req, res, next) => {
+    res.send("hello user, you are logged in and you can delete account")
+})
+
+//UPDATE
+router.put("/:id", updateUser)
+
+//DELETE
+router.delete("/:id", deleteUser)
+
+//GET
+router.get("/:id", getUser)
+
+//GET ALL
+router.get("/", getUsers)
 
 export default router

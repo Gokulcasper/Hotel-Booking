be1b7b27cd58fb7d11a3d1d3cda3b794e0209af8
Gokulcasper <ronitcasper215@gmail.com>
2022-08-27 22:56:50 +0530

    Feat : Complete Register & Login

--- api/routes/users.js
+++ api/routes/users.js
@@ -4,17 +4,17 @@ import { verifyAdmin, verifyToken, verifyUser } from "../utils/verifyToken.js";
 
 const router = express.Router()
 
-router.get("/checkauthentication", verifyToken, (req, res, next) => {
-    res.send("hello user, you are logged in")
-})
+// router.get("/checkauthentication", verifyToken, (req, res, next) => {
+//     res.send("hello user, you are logged in")
+// })
 
-router.get("/checkuser/:id", verifyUser, (req, res, next) => {
-    res.send("hello user, you are logged in and you can delete account")
-})
+// router.get("/checkuser/:id", verifyUser, (req, res, next) => {
+//     res.send("hello user, you are logged in and you can delete account")
+// })
 
-router.get("/checkadmin/:id", verifyAdmin, (req, res, next) => {
-    res.send("hello user, you are logged in and you can delete account")
-})
+// router.get("/checkadmin/:id", verifyAdmin, (req, res, next) => {
+//     res.send("hello user, you are logged in and you can delete account")
+// })
 
 //UPDATE
 router.put("/:id", updateUser)

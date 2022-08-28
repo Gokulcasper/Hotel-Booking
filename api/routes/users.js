07a81f866526f83274b6e33b0ac59b5c5c32ae3c
Gokulcasper <ronitcasper215@gmail.com>
2022-08-28 18:53:36 +0530

    Feat : Complete BackEnd Process

--- api/routes/users.js
+++ api/routes/users.js
@@ -17,15 +17,15 @@ const router = express.Router()
 // })
 
 //UPDATE
-router.put("/:id", updateUser)
+router.put("/:id", verifyUser, updateUser)
 
 //DELETE
-router.delete("/:id", deleteUser)
+router.delete("/:id", verifyUser, deleteUser)
 
 //GET
-router.get("/:id", getUser)
+router.get("/:id", verifyUser, getUser)
 
 //GET ALL
-router.get("/", getUsers)
+router.get("/", verifyAdmin, getUsers)
 
 export default router
